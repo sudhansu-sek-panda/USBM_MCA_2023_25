d2aa72633a64a248d8c66d1466cf5e03dd29e6da
Sudhansu Sekhar Panda <129420416+sudhansu-sek-panda@users.noreply.github.com>
2024-10-07 12:10:20 +0530

    string task added

--- Frontend/Javascript/Class1/index.js
+++ Frontend/Javascript/Class1/index.js
@@ -1,6 +1,8 @@
-console.log("Hyy This is MERN  project class");  
+console.log("Hyy This is MERN  project class"); 
+// console.log("My name is "+name+"! and age is "+age+" Gender is "+ gender); 
+//Hosting behaviour
 var name = "Sudhansu";
-let age = 20;
+let age = 20; //declaration, the variable is undefined.
 const gender ='M';
 
 console.log("My name is "+name+"! and age is "+age+" Gender is "+ gender);
---
+++ Frontend/Javascript/Class1/variable.js
@@ -0,0 +1,93 @@
+let data;
+console.log(typeof data);
+console.log(data);
+
+//Number 
+// Number represents both integers and floating-point numbers in Java
+// ScriptProcessorNode
+
+
+/*
+dynamically typed - java Script
+static, dynamic and hybrid web site
+integers = whole number. 
+Whole number = Positive number + Negetive Number
+floating number in Java Script
+
+
+
+
+*/
+
+let number =50;
+let decimal_number =1.0001;
+let pi = 3.141;
+let roll_no =138;
+console.log(typeof number);
+console.log(typeof decimal_number);
+console.log(typeof pi);
+console.log(typeof roll_number);
+
+//Addition 
+let num1 = 20;
+let num2 =30;
+let sum = num1+num2;
+console.log("Addition" + sum);
+//Substraction
+let sub = num1-num2;
+//Multiplication 
+let mul = num1 * num2;
+//Division
+let div = num1/num2;
+// Modulous
+let mod = num1%num2;
+console.log("Substraction "+ sub+" \n Multiplication "+mul+ " \n Division "+ div+"\n Modulus  "+ mod);
+
+let num = 28383838383838383838383834444444444444444444444;
+console.log(typeof num);
+console.log(num);
+
+let symbol1 = Symbol("sym1");
+let symbol2 = Symbol("sym1");
+console.log(symbol1 === symbol2); 
+let num5=0;
+let num6=0;
+let num7 = num5/num6;
+console.log(num7);
+
+let num8 = "Sudhansu";
+let num9 = 90;
+let num10 = num8/num9;
+console.log(num10);
+let num11 = 10;
+let num12 = 0;
+let num14 = num11/num12;
+console.log(num14);
+let num25 = num12/num11;
+console.log(num25);
+
+// Boolean
+/*
+
+
+*/
+let ismarried = false;
+let isSingle = true;
+console.log(typeof ismarried);
+console.log(typeof isSingle);
+
+let name = "Sudhansu";
+console.log(name);
+let name1 = "sudh$56";
+let nam2 = 'Sudhansu45';
+console.log(name1+nam2);
+console.log(typeof nam2);
+console.log(nam2, name1);
+
+let myName = "My name is sudhansu sekhar panda";
+console.log(`HYy ${myName} !`);
+let city = "Bhubanesway";
+let batch = "2025";
+let college = "USBM";
+console.log(`  \n \n HYy! ${myName}. I am from ${city}. I am studing in ${college}, of batch ${batch}`); //Using template literal.
+console.log(city.length);
