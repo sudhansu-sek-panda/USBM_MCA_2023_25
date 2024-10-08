705bc7c203195c519c309f9e8dc9391d00de1c7d
Sudhansu Sekhar Panda <129420416+sudhansu-sek-panda@users.noreply.github.com>
2024-10-08 12:29:22 +0530

    vowel task

---
+++ Frontend/Javascript/Class1/vowelTask.js
@@ -0,0 +1,16 @@
+const arr = ['a','e','i','o','u'];
+let alphabet = 'a';
+function vowels(array,input){
+    
+for(let i=0;i<array.length;i++){
+if(array[i] == input){
+    console.log(`The alphabate is ${ input} and vowel`)
+}
+
+}
+
+}
+ 
+
+
+vowels(arr,alphabet);
